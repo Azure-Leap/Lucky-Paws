d8caf7d4b40ff6977879f250025a052f6067e096
itstaami <eegii0977@gmail.com>
2023-04-21 16:48:12 +0800

    loginfixed

--- fe/src/components/Login/LoginPage.tsx
+++ fe/src/components/Login/LoginPage.tsx
@@ -10,62 +10,64 @@ const LoginPage = () => {
   //     "bg-gradient-to-r from-blue-800 via-purple-800 to-indigo-800";
 
   return (
-    <div className="h-4/5 w-2/4 bg-white relative overflow-hidden rounded-lg">
-      <div
-        id="signin"
-        className={`bg-white absolute top-0 left-0 h-full w-1/2 flex justify-center items-center transition-all duration-700 ease-in-out z-20 ${
-          isAnimated ? "translate-x-full opacity-0" : ""
-        }`}
-      >
-        <SigninForm />
-      </div>
-
-      <div
-        id="signup"
-        className={`absolute top-0 left-0 h-full w-1/2 flex justify-center items-center transition-all duration-700 ease-in-out ${
-          isAnimated
-            ? "translate-x-full opacity-100 z-50 animate-show"
-            : "opacity-0 z-10"
-        }`}
-      >
-        <div className="h-full w-full flex justify-center items-center">
-          <SignupForm />
-        </div>
-      </div>
-
-      <div
-        id="overlay-container"
-        className={`absolute top-0 left-1/2 w-1/2 h-full overflow-hidden transition transition-transform duration-700 ease-in-out z-100 ${
-          isAnimated ? "-translate-x-full" : ""
-        }`}
-      >
+    <div className="grid grid-cols-1 ">
+      <div className="h-screen w-full bg-white relative overflow-hidden rounded-lg">
         <div
-          id="overlay"
-          className={`bg-hero-image relative -left-full h-full w-[200%] transform transition transition-transform duration-700 ease-in-out ${
-            isAnimated ? "translate-x-1/2" : "translate-x-0"
+          id="signin"
+          className={`bg-white absolute top-0 left-0 h-full w-1/2 flex justify-center items-center transition-all duration-700 ease-in-out z-20 ${
+            isAnimated ? "translate-x-full opacity-0" : ""
+          }`}
+        >
+          <SigninForm />
+        </div>
+
+        <div
+          id="signup"
+          className={`absolute top-0 left-0 h-full w-1/2 flex justify-center items-center transition-all duration-700 ease-in-out ${
+            isAnimated
+              ? "translate-x-full opacity-100 z-50 animate-show"
+              : "opacity-0 z-10"
+          }`}
+        >
+          <div className="h-full w-full flex justify-center items-center">
+            <SignupForm />
+          </div>
+        </div>
+
+        <div
+          id="overlay-container"
+          className={`absolute top-0 left-1/2 w-1/2 h-full overflow-hidden transition transition-transform duration-700 ease-in-out z-100 ${
+            isAnimated ? "-translate-x-full" : ""
           }`}
         >
           <div
-            id="overlay-left"
-            className={`w-1/2 h-full absolute flex justify-center items-center top-0 transform -translate-x-[20%] transition transition-transform duration-700 ease-in-out ${
-              isAnimated ? "translate-x-0" : "-translate-x-[20%]"
+            id="overlay"
+            className={`bg-hero-image relative -left-full h-full w-[200%] transform transition transition-transform duration-700 ease-in-out ${
+              isAnimated ? "translate-x-1/2" : "translate-x-0"
             }`}
           >
-            <LeftOverlayContent
-              isAnimated={isAnimated}
-              setIsAnimated={setIsAnimated}
-            />
-          </div>
-          <div
-            id="overlay-right"
-            className={`w-1/2 h-full absolute flex justify-center items-center top-0 right-0 transform transition transition-transform duration-700 ease-in-out ${
-              isAnimated ? "translate-x-[20%]" : "translate-x-0"
-            }`}
-          >
-            <RightOverlayContent
-              isAnimated={isAnimated}
-              setIsAnimated={setIsAnimated}
-            />
+            <div
+              id="overlay-left"
+              className={`w-1/2 h-full absolute flex justify-center items-center top-0 transform -translate-x-[20%] transition transition-transform duration-700 ease-in-out ${
+                isAnimated ? "translate-x-0" : "-translate-x-[20%]"
+              }`}
+            >
+              <LeftOverlayContent
+                isAnimated={isAnimated}
+                setIsAnimated={setIsAnimated}
+              />
+            </div>
+            <div
+              id="overlay-right"
+              className={`w-1/2 h-full absolute flex justify-center items-center top-0 right-0 transform transition transition-transform duration-700 ease-in-out ${
+                isAnimated ? "translate-x-[20%]" : "translate-x-0"
+              }`}
+            >
+              <RightOverlayContent
+                isAnimated={isAnimated}
+                setIsAnimated={setIsAnimated}
+              />
+            </div>
           </div>
         </div>
       </div>
--- fe/src/pages/test.tsx
+++ fe/src/pages/test.tsx
@@ -1,11 +1,12 @@
 import CategoryCards from "@/components/CategoryCards/CategoryCards";
-import LoginPage from "@/components/Login/LoginPage";
+import LoginPage from "../components/Login/LoginPage";
 import React from "react";
 
 const Test = () => {
   return (
     <div>
       <CategoryCards />
+      Hello
       <LoginPage />
     </div>
   );
