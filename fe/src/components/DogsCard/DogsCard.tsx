292d5eae2827b7a19b2dc607178637cd14d324e7
Teska0075 <tushigregzen@gmail.com>
2023-04-24 01:48:37 +0800

    fixed some issues

--- fe/src/components/DogsCard/DogsCard.tsx
+++ fe/src/components/DogsCard/DogsCard.tsx
@@ -27,7 +27,7 @@ const dogs = [
 
 const Dogs = () => {
   return (
-    <div className="container mx-auto my-48 grid grid-flow-row gap-12 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="container mx-auto my-48 grid grid-flow-row gap-12 text-white sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {dogs.map((e) => (
         <Link
           href={e.link}
