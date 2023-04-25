af7b8068a1a7954d8d38d541d7f42429b07328c3
Teska0075 <tushigregzen@gmail.com>
2023-04-26 01:28:41 +0800

    breadcrumb

--- fe/src/components/Shop/Shop.tsx
+++ fe/src/components/Shop/Shop.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ShopCard from "./ShopCard";
 import Advertisement from "./Advertisement";
+import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
 
 const Shop = () => {
+  // const breadCrumbs = [
+  //   {name:"Products",link:"/products"},
+  // ]
   return (
     <div className="m-24 grid grid-cols-4 gap-5 container ">
+      {/* <Breadcrumbs breadCrumbs={breadCrumbs}/> */}
       <div className="xl:col-span-3 lg:w-full sm:col-span-4 max-sm:col-span-4 ">
         <ShopCard />
       </div>
--- fe/src/pages/layout.tsx
+++ fe/src/pages/layout.tsx
@@ -1,10 +1,15 @@
+import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 
 const Layout = ({ children }: any) => {
+  const breadCrumbs = [
+    {name:"Products",link:"/products"},
+  ]
   return (
     <div className="h-screen flex flex-col justify-between">
       <Navbar />
+      <Breadcrumbs breadCrumbs={breadCrumbs}/>
       {children}
       <Footer />
     </div>
