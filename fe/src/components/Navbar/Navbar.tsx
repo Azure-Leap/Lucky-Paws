1ee9069fe7893e63aa61c25bfecd27845019f113
eneerel <enerelboldbaatar9@gmail.com>
2023-05-02 13:58:10 +0800

    nav dropdown menu fixed

--- fe/src/components/Navbar/Navbar.tsx
+++ fe/src/components/Navbar/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
         <>
           <div className="mx-auto max-w-[85vw] px-5 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-orange-400 hover:bg-orange-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   {open ? (
