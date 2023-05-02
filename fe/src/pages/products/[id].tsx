ca43f1a08b1da57c3b5039711b88afff0efe9baf
Teska0075 <tushigregzen@gmail.com>
2023-05-02 17:09:21 +0800

    shopping cart done

--- fe/src/pages/products/[id].tsx
+++ fe/src/pages/products/[id].tsx
@@ -3,29 +3,29 @@ import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft,faAngleRight, faCheck ,faX} from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faCheck,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { useRouter } from "next/router";
 import { useProducts } from "../../hooks/useProducts";
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 
-
 const Product = ({ product }: any) => {
   const [count, setCount] = useState(1);
 
   const [slideIndex, setSlideIndex] = useState(1);
-  const [width, setWidth] = useState(0);
-  const [start, setStart] = useState(0);
-  const [change, setChange] = useState(9);
-
-  const slideRef = useRef<HTMLDivElement>(null);
 
   const [products] = useProducts();
   const router = useRouter();
+
   const {} = useRouter();
   if (router.isFallback) {
     return <div> Loading ...</div>;
-  }  
+  }
   const addCount = () => {
     setCount(count + 1);
   };
@@ -33,38 +33,10 @@ const Product = ({ product }: any) => {
     setCount(count > 1 ? -1 : -0);
   };
 
-  // Drag
-
-  const dragStart = (n: any) => {
-    setStart(n.clientX);
-  };
-  const dragOver = (n: any) => {
-    let touch = n.clientX;
-    setChange(start - touch);
-  };
-
-const dragEnd=()=>{
-    // drag left chang > 0
-    // drag right chang < 0
-    if (change > 0) {
-      slideRef.current?.scrollLeft 
-    } else {
-      slideRef.current?.scrollLeft 
-    }
-  }
-
   const plusSlides = (n: number) => {
     setSlideIndex((prev) => prev + n);
     slideShow(slideIndex + n);
   };
-
-  useEffect(() => {
-    if (!slideRef.current) return;
-    const scrollWidth = slideRef.current.scrollWidth;
-    const childrenElementCount = slideRef.current.childElementCount;
-    const width = scrollWidth / childrenElementCount;
-    setWidth(width);
-  }, []);
 
   const slideShow = (n: number) => {
     if (n > product.imgList.length) {
@@ -75,8 +47,6 @@ const dragEnd=()=>{
     }
   };
 
-
-
   const breadCrumbs = [
     { name: "Products", link: "/products" },
     { name: product.title, link: "" },
@@ -84,7 +54,7 @@ const dragEnd=()=>{
 
   return (
     <div className="bg-[#fff3d3]">
-      <Breadcrumbs breadCrumbs={breadCrumbs}/>
+      <Breadcrumbs breadCrumbs={breadCrumbs} />
       <div className=" container mx-auto my-10 p-5 mt-7 rounded-xl">
         <div className="grid grid-cols-2  bg-white m-5 rounded-3xl">
           <div className="product-page-img w-full h-auto xl:col-span-1 sm:col-span-2 max-sm:col-span-2 ">
@@ -112,15 +82,19 @@ const dragEnd=()=>{
 
               <a
                 className="prev text-orange-500 absolute top-1/2 -translate-y-1/2 left-4  text-4xl cursor-pointer"
-                onClick={() => {plusSlides(-1)
-                console.log("Prev",slideIndex)}}
+                onClick={() => {
+                  plusSlides(-1);
+                  console.log("Prev", slideIndex);
+                }}
               >
                 &#10094;
               </a>
               <a
                 className="next absolute top-1/2 -translate-y-1/2 right-4 text-orange-500 text-4xl cursor-pointer"
-                onClick={() => {plusSlides(1)
-                console.log("Next",slideIndex)}}
+                onClick={() => {
+                  plusSlides(1);
+                  console.log("Next", slideIndex);
+                }}
               >
                 &#10095;
               </a>
@@ -129,18 +103,12 @@ const dragEnd=()=>{
             <div
               className="slider-img w-full h-1/4 overflow-x-scroll scrollbar-hide flex snap-x"
               draggable={true}
-              ref={slideRef}
-              onDragStart={dragStart}
-              onDragOver={dragOver}
-              onDragEnd={dragEnd}
             >
               {product.imgList.map((image: any, index: number) => (
                 <div
                   key={index}
                   className={`bg-blue-400 slider-box w-1/3 bottom-0 flex-none h-full cursor-pointer inline opacity-100 snap-end ${
-                    index === slideIndex - 1
-                      ? "border-4 border-green-500"
-                      : ""
+                    index === slideIndex - 1 ? "border-4 border-green-500" : ""
                   }`}
                   onClick={() => setSlideIndex(index + 1)}
                 >
@@ -165,8 +133,21 @@ const dragEnd=()=>{
             <div className="mt-6 flex">
               <h3 className="font-bold">Price :</h3> {product.price}
             </div>
-            <div className="text-lg mt-6">In Stock  {product.inStock === 0 ? <FontAwesomeIcon icon={faX} className="text-red-500 text-xl" /> : <FontAwesomeIcon icon={faCheck} className="text-green-500 text-xl"/> }</div>
-            <div className="mt-6 text-lg"><h2 className="text-2xl font-bold mb-3">Description:</h2> {product.detail}</div>
+            <div className="text-lg mt-6">
+              In Stock{" "}
+              {product.inStock === 0 ? (
+                <FontAwesomeIcon icon={faX} className="text-red-500 text-xl" />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className="text-green-500 text-xl"
+                />
+              )}
+            </div>
+            <div className="mt-6 text-lg">
+              <h2 className="text-2xl font-bold mb-3">Description:</h2>{" "}
+              {product.detail}
+            </div>
             <div className="font-bold text-2xl mt-6">Net Weight</div>
             <div className="mt-2 text-lg"> {product.netWeight}</div>
             <div className="font-bold text-2xl mt-6">Type </div>
@@ -187,40 +168,43 @@ const dragEnd=()=>{
                 <FontAwesomeIcon icon={faAngleRight} />
               </button>
             </div>
-          <div className="grid grid-cols-6 my-12">
-            <button className="bg-orange-400 rounded-lg text-white font-bold h-10 border-2 border-orange-400 border-opacity-75  hover:bg-white hover:text-orange-400 hover:scale-110 xl:col-span-2 sm:col-span-2 max-sm:col-span-2">
-              Add To Card
-            </button>
-            <Link href={`/payment`} passHref>
-            <button className="bg-white rounded-lg mx-8 w-28 text-orange-400 font-bold h-10 border-2 border-orange-400 border-opacity-75  hover:bg-orange-400 hover:text-white hover:scale-110 xl:col-span-2 sm:col-span-2 max-sm:col-span-2">
-              Buy
-            </button>
-            </Link>
-          </div>
+            <div className="grid grid-cols-6 my-12">
+              <button className="bg-orange-400 rounded-lg text-white font-bold h-10 border-2 border-orange-400 border-opacity-75  hover:bg-white hover:text-orange-400 hover:scale-110 xl:col-span-2 sm:col-span-2 max-sm:col-span-2">
+                Add To Card
+              </button>
+              <Link href={`/payment`} passHref>
+                <button className="bg-white rounded-lg mx-8 w-28 text-orange-400 font-bold h-10 border-2 border-orange-400 border-opacity-75  hover:bg-orange-400 hover:text-white hover:scale-110 xl:col-span-2 sm:col-span-2 max-sm:col-span-2">
+                  Buy
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
         <div className=" grid grid-cols-3 m-5  ">
           {products?.slice(0, 3).map((product: any, index: number) => (
             <Link key={index} href={`products/${product._id}`} passHref>
-            <div key={index} className="group bg-white hover:scale-105 shadow-[0_8px_16px_rgba(132,74,20,0.25)] rounded-xl mx-6 mt-16 max-sm:col-span-3 sm:col-span-3  md:col-span-3 lg:col-span-1 ">
-              <div className="group grid grid-cols-2">
-                <div className="max-sm:col-span-1 col-span-1 md:col-span-1 xl:col-span-1 ">
-                  <Image
-                    src={product.imgList[0].src}
-                    alt="productsPhoto"
-                    width={300}
-                    height={100}
-                    className="h-[200px] w-full rounded-lg object-contain "
-                  />
-                </div>
-                <div className="max-sm:col-span-1 sm:col-span-1 md:col-span-1 xl:col-span-1 m-auto">
-                  <div className=" max-sm:text-2xl sm:text-2xl md:text-xl text-center p-1 ">
-                    {product.title}
+              <div
+                key={index}
+                className="group bg-white hover:scale-105 shadow-[0_8px_16px_rgba(132,74,20,0.25)] rounded-xl mx-6 mt-16 max-sm:col-span-3 sm:col-span-3  md:col-span-3 lg:col-span-1 "
+              >
+                <div className="group grid grid-cols-2">
+                  <div className="max-sm:col-span-1 col-span-1 md:col-span-1 xl:col-span-1 ">
+                    <Image
+                      src={product.imgList[0].src}
+                      alt="productsPhoto"
+                      width={300}
+                      height={100}
+                      className="h-[200px] w-full rounded-lg object-contain "
+                    />
                   </div>
-                  <div className="p-1">{product.price}</div>
+                  <div className="max-sm:col-span-1 sm:col-span-1 md:col-span-1 xl:col-span-1 m-auto">
+                    <div className=" max-sm:text-2xl sm:text-2xl md:text-xl text-center p-1 ">
+                      {product.title}
+                    </div>
+                    <div className="p-1">{product.price}</div>
+                  </div>
                 </div>
               </div>
-            </div>
             </Link>
           ))}
         </div>
