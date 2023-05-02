94159127042232c8e3f2ae75f198e91d557cf863
Teska0075 <tushigregzen@gmail.com>
2023-05-02 19:18:44 +0800

    sidebar started

---
+++ fe/src/pages/profile/index.tsx
@@ -0,0 +1,12 @@
+import SideBar from "@/components/SideBar/SideBar";
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className=" w-full h-full grid grid-cols-12">
+      <SideBar />
+
+      <section className="bg-red-300 col-span-10">asdasd</section>
+    </div>
+  );
+}
