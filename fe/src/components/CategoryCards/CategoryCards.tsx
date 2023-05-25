5a0ebc1442a89674cd9d2bc097d962b638a6ef0d
Teska0075 <tushigregzen@gmail.com>
2023-05-26 04:06:02 +0800

    animations added

--- fe/src/components/CategoryCards/CategoryCards.tsx
+++ fe/src/components/CategoryCards/CategoryCards.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faDog,
@@ -19,26 +20,64 @@ export default function CategoryCards(): any {
     { icon: faShoppingCart, link: "/products", title: "Shopping" },
     { icon: faPaw, link: "/blog", title: "Blogs" },
   ];
+
+  const cardVariant = {
+    hidden: {
+      opacity: 0,  
+      y: '100vw',  
+    },
+    visible: {
+      opacity: 1,  
+      y: 0,  
+      transition: {
+        duration: 1,  
+        ease: 'easeInOut',  
+      },
+    },
+  };
+
+  const hoverVariant = {
+    hover: {
+      scale: 1.05,  
+    },
+    pressed: {
+      scale: 0.98,  
+    },
+  }
+
   return (
-    <div className="container mx-auto p-5  mb-24 grid grid-flow-row gap-12 text-neutral-600 max-sm:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <motion.div className="container mx-auto p-5  mb-24 grid grid-flow-row gap-12 text-neutral-600 max-sm:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+    initial="hidden"
+    animate="visible"
+    whileHover="hover"
+    whileTap="pressed"
+    variants={cardVariant}
+    >
       {categoryItems.map((e, index) => (
-        <Link
-          key={index}
-          href={e.link}
-          className="pt-10 pb-6 flex flex-col gap-10 text-center justify-center rounded-3xl shadow-[0_8px_16px_rgba(132,74,20,0.25)] hover:scale-105"
+        <motion.div
+          whileHover="hover"
+          whileTap="pressed"
+          variants={hoverVariant}
         >
-          <FontAwesomeIcon
-            icon={e.icon}
-            className="text-orange-500 text-8xl"
-          />
-          <div
+
+          <Link
             key={index}
-            className={`text-center xl:text-4xl max-sm:text-xl font-extrabold text-orange-500 ${fredokaOne.className}`}
+            href={e.link}
+            className="pt-10 pb-6 flex flex-col gap-10 text-center justify-center rounded-3xl shadow-[0_8px_16px_rgba(132,74,20,0.25)] hover:scale-105"
           >
-            {e.title}
-          </div>
-        </Link>
+            <FontAwesomeIcon
+              icon={e.icon}
+              className="text-orange-500 text-8xl"
+            />
+            <div
+              key={index}
+              className={`text-center xl:text-4xl max-sm:text-xl font-extrabold text-orange-500 ${fredokaOne.className}`}
+            >
+              {e.title}
+            </div>
+          </Link>
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 }
--- fe/src/pages/products/index.tsx
+++ fe/src/pages/products/index.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import _ from "lodash";
+import { motion } from "framer-motion";
 
 import { useProducts } from "../../hooks/useProducts";
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import Pagination from "../../components/Shop/Pagination";
 import { ShopFilter } from "@/components/Shop/ShopFilter";
+
 
 const Products = () => {
   const { products } = useProducts();
@@ -47,6 +49,45 @@ const Products = () => {
     setFilteredList(newFilter);
   }
 
+  const containerLeft = {
+    hidden: {
+      opacity: 0,  
+      x: '-100vw',  
+    },
+    visible: {
+      opacity: 1,  
+      x: 0,  
+      transition: {
+        duration: 1,  
+        ease: 'easeInOut',  
+      },
+    },
+  };
+
+  const containerRight = {
+    hidden: {
+      opacity: 0,  // Starts with opacity 0
+      x: '100vw',  // Starts outside the right edge of the screen
+    },
+    visible: {
+      opacity: 1,  // Fades in with opacity 1
+      x: 0,  // Moves to the center of the screen
+      transition: {
+        duration: 1,  // Duration of the animation (in seconds)
+        ease: 'easeInOut',  // Easing function for a smooth animation
+      },
+    },
+  }
+
+  const hoverVariant = {
+    hover: {
+      scale: 1.05,  
+    },
+    pressed: {
+      scale: 0.98,  
+    },
+  }
+
   const breadCrumbs = [{ name: "Products", link: "" }];
 
   if (router.isFallback) {
@@ -56,17 +97,30 @@ const Products = () => {
     <div className="bg-[#fff3d3]">
       <Breadcrumbs breadCrumbs={breadCrumbs} />
       <div className="m-auto container grid grid-cols-5">
-        <div className="md:col-span-1 bg-white  md:aspect-[9/12] rounded-lg m-5 sm:col-span-5 max-sm:col-span-5 shadow-[0_8px_16px_rgba(132,74,20,0.25)]">
+        <motion.div className="md:col-span-1 bg-white  md:aspect-[9/12] rounded-lg m-5 sm:col-span-5 max-sm:col-span-5 shadow-[0_8px_16px_rgba(132,74,20,0.25)]"
+          initial="hidden"
+          animate="visible"
+          variants={containerLeft}
+        >
           <ShopFilter
             handleCategory={handleCategory}
             handleType={handleType}
             handleAll={handleAll}
           />
-        </div>
-        <div className="mx-auto md:col-span-4 sm:col-span-5 max-sm:col-span-5 grid xl:grid-cols-3 sm:grid-cols-3 md:grid-cols-2  max-sm:grid-cols-1 p-2">
+        </motion.div>
+        <motion.div className="mx-auto md:col-span-4 sm:col-span-5 max-sm:col-span-5 grid xl:grid-cols-3 sm:grid-cols-3 md:grid-cols-2  max-sm:grid-cols-1 p-2"
+          initial="hidden"
+          animate="visible"
+          variants={containerRight}
+        >
           {filteredList?.map((product: any, idx: number) => (
-            <Link key={idx} href={`products/${product._id}`} passHref>
-              <div className="group bg-white hover:scale-110  shadow-[0_8px_16px_rgba(132,74,20,0.25)] rounded-3xl m-3">
+            <motion.div
+            whileHover="hover"
+            whileTap="pressed"
+            variants={hoverVariant}
+            >
+              <Link key={idx} href={`products/${product._id}`} passHref>
+              <div className="group bg-white shadow-[0_8px_16px_rgba(132,74,20,0.25)] rounded-3xl m-3">
                 <div className="group grid grid-cols-2">
                   <div className="max-sm:col-span1 sm:col-span-2 md:col-span-2 xl:col-span-2 ">
                     <img
@@ -86,10 +140,17 @@ const Products = () => {
                 </div>
               </div>
             </Link>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
-      <Pagination />
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={containerRight}
+      >
+        <Pagination />
+      </motion.div>
     </div>
   );
 };
